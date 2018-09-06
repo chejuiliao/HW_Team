cae2129768de691ee6566652f8f91e6810a5f3a3
Jerryliao <chejuiliao@users.noreply.github.com>
2018-09-06 17:16:54 -0400

    Initial commit
    There are some missing values in the data, so we need to create a complete time sequence and interpolate the value using na.approx() to our well_ts

---
+++ HW_TS_Well_Prediction_2.R
@@ -0,0 +1,151 @@
+library(readxl)
+library(lubridate)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(zoo)
+
+na.approx()
+
+rm(list=ls())
+# import well data
+well <- read_excel("C:/Users/Jerry/Documents/MSA18/Time_Series/HW/PB-1680_T.xlsx", sheet = 3)
+
+# change time data into character and make the hourly time frame
+well$date <- as.character(well$date)
+well$time <- as.character(well$time)
+well$hour <- paste(well$date, str_sub(well$time, 12, 13))
+
+# change the time data into UTC time zone
+well$hour <- paste0(well$hour, ":00:00", " ", well$tz_cd)
+well$hour <- as.POSIXct(well$hour)
+attributes(well$hour)$tzone <- "UTC"
+
+well <- well %>%
+  arrange(hour)
+
+which(diff(well$hour) == 9075600)
+well[32570:32580,]
+# aggregate the data on hour basis
+well_agg <- well %>%
+  group_by(hour) %>%
+  summarise(Well_ft_mean = mean(Well_ft, na.rm = TRUE),
+            Corrected_mean = mean(Corrected, na.rm = TRUE)) 
+
+# arrange the time data to create monthly basis
+month_agg <- well_agg
+month_agg$hour <- as.POSIXct(well_agg$hour)
+month_agg <- cbind(month = paste(as.character(year(well_agg$hour)), as.character(month(well_agg$hour))),well_agg)
+month_agg$hour <- NULL
+month_agg[,1] <- paste(month_agg[,1], "01")
+month_agg[,1] <- as.Date(month_agg[,1], "%Y %m %d")
+
+# aggregate the data on monthly basis
+month_agg <- month_agg %>%
+  group_by(month_agg[,1]) %>%
+  summarise(Well_ft_mean = mean(Well_ft_mean, na.rm = TRUE), Corrected_mean = mean(Corrected_mean, na.rm = TRUE))
+colnames(month_agg)[1] <- "month"
+
+View(month_agg)
+
+month_agg[44:58, ]
+
+# order the data by month
+month_agg <- month_agg[order(month_agg$month),]
+
+# use well_ft_mean as time series data because all the values are positive
+well_ts <- ts(as.numeric(as.data.frame(month_agg)[,2]), start = c(2007, 10), end = c(2018, 6), frequency = 12)
+
+# Use a holdout data set
+training <- subset(well_ts, end = length(well_ts) - 6)
+test <- subset(well_ts, start = length(well_ts) - 5)
+
+
+# generate prediction by using different model
+HW_M_Train <- hw(training, seasonal = "multiplicative",initial='optimal', h = 6)
+HW_A_Train <- hw(training, seasonal = "additive",initial='optimal', h = 6)
+SES_Train <- ses(training, initial = "optimal", h=6)
+HOLT_Train <- holt(training,initial='optimal', h = 6)
+
+# store the different model results in a list
+ls_Model <- list(HW_M = HW_M_Train, HW_A = HW_A_Train, SES = SES_Train, HOLT = HOLT_Train)
+
+# calculate MAPE
+for(i in 1:length(ls_Model)){
+  error = test - ls_Model[[i]]$mean
+  ls_Model[[i]]$MAPE = mean(abs(error)/abs(test))
+}
+
+# look for the least MAPE
+for(i in 1:length(ls_Model)){
+  print(ls_Model[[i]]$MAPE)
+}
+
+
+# check the plots (if aggregating by month changes the seasonality)
+ggplot(month_agg, aes(month, Well_ft_mean)) + geom_line() +
+  xlab("time") + ylab("well_ft_mean")
+
+# decomposition
+well_ft <- ts(month_agg$Well_ft_mean, start = 2007, frequency = 12)
+model <- stl(well_ft, s.window = 7, na.action = na.approx)
+plot(model)
+
+HW_stl <- stl(ls_Model$HW_M$fitted, s.window = 7, na.action = na.approx)
+plot(HW_stl)
+
+df_HW_stl <- data.frame(HW_stl$time.series)
+df_HW_stl <- cbind(df_HW_stl, month = month_agg$month[1:(nrow(month_agg) - 6)])
+# change the model time series into data frame
+df_ts <- data.frame(model$time.series)
+df_ts <- cbind(df_ts, month = month_agg$month)
+df_ts <- df_ts[1:(nrow(df_ts) - 6),]
+df_ts <- cbind(df_ts, HW_seasonal = df_HW_stl$seasonal, HW_trend = df_HW_stl$trend, HW_remainder = df_HW_stl$remainder)
+View(df_ts)
+
+
+# create the plot for the trend
+ggplot(df_ts, aes(month)) + 
+  geom_line(aes(y = trend, colour = "Actual")) + 
+  geom_line(aes(y = HW_trend, colour = "Prediction")) +
+  xlab("Date") +
+  ylab("Well ft") +
+  labs(title = "Actual vs. Model Trend Decomposition", size = 15) +
+  theme(legend.title = element_blank())
+  
+# create the plot for the seasonality
+ggplot(df_ts, aes(month)) + 
+  geom_line(aes(y = seasonal, colour = "Actual")) + 
+  geom_line(aes(y = HW_seasonal, colour = "Prediction")) +
+  xlab("Date") +
+  ylab("Well ft") +
+  labs(title = "Actual vs. Model Seasonality Decomposition", size = 15) +
+  theme(legend.title = element_blank())
+
+df_prediction <- data.frame(cbind(month = month_agg$month[(nrow(month_agg)-5):nrow(month_agg)], test = test, HW_M = ls_Model$HW_M$mean))
+df_prediction$month <- as.Date(df_prediction$month)
+
+ls_Model
+
+
+# create the plot for the prediction
+ggplot(df_prediction, aes(month)) + 
+  geom_line(aes(y = test, colour = "Actual")) +
+  geom_line(aes(y = HW_M, colour = "Prediction")) +
+  xlab("Date") +
+  ylab("Well ft") +
+  labs(title = "Well ft Actual vs. Prediction", size = 15) +
+  theme(legend.title = element_blank())
+
+# ESM forecast 6 month out
+autoplot(ls_Model$HW_M) +
+  autolayer(fitted(ls_Model$HW_M), series = "Fitted") +
+  ylab("Well ft") +
+  xlab("Date") +
+  labs(title = "Well ft ESM Forecast for 6 months", size = 15) +
+  theme(legend.title = element_blank())
+  
+
+
+
+
